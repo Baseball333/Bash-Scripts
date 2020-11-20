120cfdc5d105d39f163d7a8943ee58c52860fb55
Baseball333 <52107974+Baseball333@users.noreply.github.com>
2020-11-19 21:07:07 -0800

    Create square.sh

---
+++ square.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+:"
+"This script calculates the square of 5.
+"
+((area = 5*5))
+>>>EOF
+
+# Remember to configure with chmod a+x and bash square.sh
+# These are extremely simple scripts
+
+
