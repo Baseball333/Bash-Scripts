c9049fa249c3fa7b5a7389e5a303a67f47ffcc40
Baseball333 <52107974+Baseball333@users.noreply.github.com>
2020-11-19 21:43:43 -0800

    Create while.sh

---
+++ while.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# A while loop
+valid = true
+count = 1
+while [[ $count -eq 5 ]];
+then
+break
+fi
+((count++))
+done
+
+# Make sure to configure the script with chmod a+x and bash while.sh
+
+
+
+
+
+
+
