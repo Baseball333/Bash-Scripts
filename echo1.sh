e774186fff99b9a9718495dfccafdc70cce98c6c
Baseball333 <52107974+Baseball333@users.noreply.github.com>
2020-11-19 20:50:27 -0800

    Create echo1.sh

---
+++ echo1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Printing text with numbers"
+echo -n "Printing text without newline"
+echo -e "\nRemoving t\backslash \t characters \n"
+
+# Make sure to configure it with chmod a+x and bash echo1.sh
