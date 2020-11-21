4bb765cfcd165ae0b476e55a75735df851ddbb20
Baseball333 <52107974+Baseball333@users.noreply.github.com>
2020-11-20 19:36:18 -0800

    Create userinput.sh

---
+++ userinput.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo -n "Enter Something:"
+read something
+
+echo "You entered: $something"
+
+# Make sure to configure the script with chmod a+x userinput.sh and bash userinput.sh
+
+
